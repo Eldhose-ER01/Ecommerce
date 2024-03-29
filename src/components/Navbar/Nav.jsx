5c29c5f76b850705f04815df3d414e7073f92124
Eldhose ER <eldhoseer16@gmail.comgit config --global user.email eldhoseer16@gmail.comgit config --global user.name Eldhose>
2024-03-29 14:16:19 +0530

    Chnage Nav Link

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,20 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Nav() {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-400">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
+        <div className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREi-555dui7zkXHv351AVM32uUFegLz7JQJRWYV_Tnm_rtUE1qqGUdJeQWxlbus7vy-Nk&usqp=CAU"
             className="h-20 w-20 ml-2 rounded-full"
+            alt="Logo"
           />
-
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             MyShop
           </span>
-        </a>
+        </div>
         <button
           data-collapse-toggle="navbar-default"
           type="button"
@@ -32,9 +32,9 @@ export default function Nav() {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M1 1h15M1 7h15M1 13h15"
             />
           </svg>
@@ -42,37 +42,37 @@ export default function Nav() {
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-400 dark:border-gray-700">
             <li>
-              <a
-                href="/"
+              <Link
+                to="/"
                 className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                 aria-current="page"
               >
                 ProductList
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/productcategory"
+              <Link
+                to="/productcategory"
                 className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-100 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 ProductCategory
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/listcategory"
+              <Link
+                to="/listcategory"
                 className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-100 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 CategoryList
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/userlist"
+              <Link
+                to="/userlist"
                 className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-100 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 UserList
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
